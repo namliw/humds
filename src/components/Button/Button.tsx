a3b85e8143e3e8ef41206718c0564b2be31c28b1
Willman Corcho <will.corcho@estimateone.com>
2021-07-26 17:32:54 +1000

    Add base for Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React, {ButtonHTMLAttributes} from 'react';
+import {joinClassNames} from "../../utils";
+import * as styles from './styles.css';
+
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    label: string;
+}
+
+export const Button: React.VFC<ButtonProps> = ({ label, className, ...props }) => (
+    <button className={joinClassNames(styles.button, className)} {...props}>
+        {label}
+    </button>
+);
