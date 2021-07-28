bde952a05054ac7d70df9ebc97e2c2f4efdf6ab2
Willman Corcho <will.corcho@estimateone.com>
2021-07-29 08:40:31 +1000

    Set sb to webpack5 properly?

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,4 @@
 module.exports = {
-  core: {
-    builder: 'webpack5',
-  },
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -9,5 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  "core": {
+    "builder": "webpack5"
+  }
 }
