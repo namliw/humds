f1f5533ecc4fbf70a197041cf08d30d6685c1efe
Willman Corcho <will.corcho@estimateone.com>
2021-07-29 09:30:31 +1000

    linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {BasePictoCard, PictoCardGroup} from "./components/PictoCard";
-import Img580x387 from "./assets/img/img-580x387.jpg";
+import { BasePictoCard, PictoCardGroup } from './components/PictoCard';
+import Img580x387 from './assets/img/img-580x387.jpg';
 
 const ExamplePictoCard = () => <BasePictoCard imgAttributes={{ src: Img580x387, alt: 'Img' }}>Title goes here</BasePictoCard>;
 
-const App = () => <div>Hello, World!
+const App = () => (
+  <div>
+    Hello, World!
     <PictoCardGroup>
-        <ExamplePictoCard />
-        <ExamplePictoCard />
-        <ExamplePictoCard />
-        <ExamplePictoCard />
+      <ExamplePictoCard />
+      <ExamplePictoCard />
+      <ExamplePictoCard />
+      <ExamplePictoCard />
     </PictoCardGroup>
-</div>;
+  </div>
+);
 
 export default App;
