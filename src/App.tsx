f418c9eed21fdf764aab6917b1de01be98c04680
Willman Corcho <will.corcho@estimateone.com>
2021-07-29 09:30:17 +1000

    breaking stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import {BasePictoCard, PictoCardGroup} from "./components/PictoCard";
+import Img580x387 from "./assets/img/img-580x387.jpg";
 
-const App = () => <div>Hello, World!</div>;
+const ExamplePictoCard = () => <BasePictoCard imgAttributes={{ src: Img580x387, alt: 'Img' }}>Title goes here</BasePictoCard>;
+
+const App = () => <div>Hello, World!
+    <PictoCardGroup>
+        <ExamplePictoCard />
+        <ExamplePictoCard />
+        <ExamplePictoCard />
+        <ExamplePictoCard />
+    </PictoCardGroup>
+</div>;
 
 export default App;
