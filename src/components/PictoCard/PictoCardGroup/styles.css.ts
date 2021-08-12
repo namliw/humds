0e1b0914ce3e0490e0afb1c7c07852cab3d21561
Cassandra Tam <cassandra.tam@estimateone.com>
2021-08-12 23:10:14 +1000

    feat: add extra media query styles to PictoCardGroup

--- src/components/PictoCard/PictoCardGroup/styles.css.ts
+++ src/components/PictoCard/PictoCardGroup/styles.css.ts
@@ -4,16 +4,15 @@ import { vars, tokens } from '../../../tokens/index.css';
 export const pictoCardGroup = style({
   display: 'grid',
   gridTemplateRows: 'auto',
-  gridTemplateColumns: '1fr', // Mobile
+  gridTemplateColumns: '1fr',
   rowGap: vars.spacing.space40,
   columnGap: vars.spacing.space40,
   '@media': {
     [tokens.mediaQuery.large]: {
       gridTemplateColumns: 'repeat(3, 1fr)',
     },
-    // @todo Enable this once multiple media queries works
-    // [tokens.mediaQuery.medium]: {
-    //   gridTemplateColumns: 'repeat(2, 1fr)',
-    // },
+    [tokens.mediaQuery.medium]: {
+      gridTemplateColumns: 'repeat(2, 1fr)',
+    },
   },
 });
