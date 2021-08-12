d0037e933d59d64088892695e540a5cce352b17e
Cassandra Tam <cassandra.tam@estimateone.com>
2021-08-12 23:29:03 +1000

    chore: Add new viewports to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,22 @@
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const customViewports = {
+  breakpointSm: {
+    name: 'Breakpoint Small (576px)',
+    styles: {
+      width: '576px',
+      height: '800px',
+    },
+  },
+  breakpointMd: {
+    name: 'Breakpoint Medium (768px)',
+    styles: {
+      width: '768px',
+      height: '800px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +25,10 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: {
+      ...customViewports,
+      ...INITIAL_VIEWPORTS,
+    },
+  }
 }
